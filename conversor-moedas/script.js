a216014edf886f318f4dc62b21e2894419595f74
Jessica Kopps <jessicakopps@gmail.com>
2021-09-20 17:46:49 -0300

    Update script.js

--- conversor-moedas/script.js
+++ conversor-moedas/script.js
@@ -1,42 +1,27 @@
 function ConverterDolar() {
-  var valorElemento = document.getElementById("valor");
-  var valor = valorElemento.value;
-  var valorEmDolarNumerico = parseFloat(valor);
-
-  var valorEmReal = valorEmDolarNumerico * 5.25;
+  var valorElemento = parseFloat(document.getElementById("valor").value);
+  var valorEmReal = valorElemento * 5.25;
 
   var elementoValorConvertido = document.getElementById("valorConvertido");
-  var valorConvertido = `O valor de $${valor} em Real é R$${valorEmReal.toFixed(
-    2
-  )}`;
+  var valorConvertido = `O valor de $${valor} em Real é R$${valorEmReal.toFixed(2)}`;
   elementoValorConvertido.innerHTML = valorConvertido;
 }
 
 function ConverterEuro() {
-  var valorElemento = document.getElementById("valor");
-  var valor = valorElemento.value;
-  var valorEmEuroNumerico = parseFloat(valor);
-
-  var valorEmReal = valorEmEuroNumerico * 6.18;
+  var valorElemento = parseFloat(document.getElementById("valor").value);
+  var valorEmReal = valorElemento * 6.18;
 
   var elementoValorConvertido = document.getElementById("valorConvertido");
-  var valorConvertido = `O valor de €${valor} em Real é R$${valorEmReal.toFixed(
-    2
-  )}`;
+  var valorConvertido = `O valor de €${valor} em Real é R$${valorEmReal.toFixed(2)}`;
   elementoValorConvertido.innerHTML = valorConvertido;
 }
 
 function ConverterBitcoin() {
-  var valorElemento = document.getElementById("valor");
-  var valor = valorElemento.value;
-  var valorEmBitcoinNumerico = parseFloat(valor);
-
-  var valorEmReal = valorEmBitcoinNumerico * 252110.74;
+  var valorElemento = parseFloat(document.getElementById("valor").value);
+  var valorEmReal = valorElemento * 252110.74;
 
   var elementoValorConvertido = document.getElementById("valorConvertido");
-  var valorConvertido = `O valor de ฿${valor} em Real é R$${valorEmReal.toFixed(
-    2
-  )}`;
+  var valorConvertido = `O valor de ฿${valor} em Real é R$${valorEmReal.toFixed(2)}`;
   elementoValorConvertido.innerHTML = valorConvertido;
 }
 
