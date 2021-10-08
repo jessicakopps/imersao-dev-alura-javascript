f7892c762799a13672218393529182555bf4b71f
Jessica Kopps <jessicakopps@gmail.com>
2021-10-08 12:19:12 -0300

    images

--- super-trunfo/script.js
+++ super-trunfo/script.js
@@ -1,153 +1,153 @@
 // Declaração das cartas
 cartas = [
   {
-    Nome: "Herína",
+    Nome: "Heroína",
     Atributos: { HP: 66, Força: 50, Defesa: 50, Elemento: 50 },
-    img: "https://static.wikia.nocookie.net/mana/images/f/f4/Heroine_%28LoM_Artwork%29.png/revision/latest?cb=20150926052948",
+    img: "./images/Heroine.png",
     moldura: "card.png"
   },
   {
     Nome: "Herói",
     Atributos: { HP: 66, Força: 50, Defesa: 50, Elemento: 50 },
-    img: "https://static.wikia.nocookie.net/mana/images/c/c1/Hero_%28LoM_Artwork%29.png/revision/latest?cb=20150926052916",
+    img: "./images/Hero.png",
     moldura: "card.png"
   },
   {
     Nome: "Niccolo",
     Atributos: { HP: 50, Força: 60, Defesa: 70, Elemento: 30 },
-    img: "https://static.wikia.nocookie.net/mana/images/7/78/Niccolo_%28LoM_Artwork%29.png/revision/latest/scale-to-width-down/350?cb=20150912153039",
+    img: "./images/Niccolo.png",
     moldura: "card.png"
   },
   {
     Nome: "Elazul ",
     Atributos: { HP: 70, Força: 80, Defesa: 42, Elemento: 82 },
-    img: "https://static.wikia.nocookie.net/mana/images/7/70/Elazul_%28LoM_Artwork%29.png/revision/latest/smart/width/250/height/250?cb=20150912011038",
+    img: "./images/Elazul.png",
     moldura: "card.png"
   },
   {
     Nome: "Lady Blackpearl",
     Atributos: { HP: 72, Força: 82, Defesa: 42, Elemento: 82 },
-    img: "https://static.wikia.nocookie.net/mana/images/9/9e/Lady_Blackpearl_%28LoM_Artwork%29.png/revision/latest?cb=20150918182430",
+    img: "./images/Lady_Blackpearl.png",
     moldura: "card.png"
   },
   {
     Nome: "Sandra",
     Atributos: { HP: 65, Força: 70, Defesa: 50, Elemento: 40 },
-    img: "https://static.wikia.nocookie.net/mana/images/7/7b/Sandra_%28LoM_Artwork%29.png/revision/latest?cb=20150919134313",
+    img: "./images/Sandra.png",
     moldura: "card.png"
   },
   {
     Nome: "Lisa",
     Atributos: { HP: 60, Força: 20, Defesa: 12, Elemento: 80 },
-    img: "https://static.wikia.nocookie.net/mana/images/b/b9/Lisa_%28LoM_Artwork%29.png/revision/latest/scale-to-width-down/350?cb=20150913061222",
+    img: "./images/Lisa.png",
     moldura: "card.png"
   },
   {
     Nome: "Bud",
     Atributos: { HP: 60, Força: 20, Defesa: 12, Elemento: 80 },
-    img: "https://static.wikia.nocookie.net/mana/images/a/a7/Bud_%28LoM_Artwork%29.png/revision/latest/scale-to-width-down/303?cb=20150913061020",
+    img: "./images/Bud.png",
     moldura: "card.png"
   },
   {
     Nome: "Daena ",
     Atributos: { HP: 75, Força: 82, Defesa: 41, Elemento: 60 },
-    img: "https://static.wikia.nocookie.net/mana/images/7/76/Daena_%28LoM_Artwork%29.png/revision/latest/scale-to-width-down/350?cb=20150912004139",
+    img: "./images/Daena.png",
     moldura: "card.png"
   },  
   {
     Nome: "Larc",
     Atributos: { HP: 60, Força: 82, Defesa: 20, Elemento: 60 },
-    img: "https://static.wikia.nocookie.net/mana/images/8/84/Larc_the_Centaur.gif/revision/latest/scale-to-width-down/127?cb=20180428225221",
+    img: "./images/Larc_the_Centaur.png",
     moldura: "card.png"
   },
   {
     Nome: "Sierra",
     Atributos: { HP: 45, Força: 85, Defesa: 46, Elemento: 52 },
-    img: "https://static.wikia.nocookie.net/mana/images/f/f3/Sierra_%28LoM_Artwork%29.png/revision/latest/scale-to-width-down/284?cb=20150912003934",
+    img: "./images/Sierra.png",
     moldura: "card.png"
   },
   {
     Nome: "Escad",
     Atributos: { HP: 62, Força: 85, Defesa: 45, Elemento: 52 },
-    img: "https://static.wikia.nocookie.net/mana/images/8/8f/Youth_Escad_%28LoM_Artwork%29.png/revision/latest?cb=20150919134633",
+    img: "./images/Youth_Escad.png",
     moldura: "card.png"
   },  
   {
     Nome: "Elle",
     Atributos: { HP: 65, Força: 1, Defesa: 25, Elemento: 100 },
-    img: "https://static.wikia.nocookie.net/mana/images/d/d3/Elle_%28LoM_Artwork%29.png/revision/latest/scale-to-width-down/289?cb=20150913082557",
+    img: "./images/Elle.png",
     moldura: "card.png"
   },  
   {
     Nome: "Count Dovula",
     Atributos: { HP: 70, Força: 80, Defesa: 60, Elemento: 80 },
-    img: "https://static.wikia.nocookie.net/mana/images/c/c4/Count_Dovula_%28LoM_Artwork%29.png/revision/latest/scale-to-width-down/350?cb=20150919133151",
+    img: "./images/Count_Dovula.png",
     moldura: "card.png"
   },
   {
     Nome: "Kristie",
     Atributos: { HP: 80, Força: 80, Defesa: 80, Elemento: 80 },
-    img: "https://static.wikia.nocookie.net/mana/images/2/2b/Kristie_%28LoM_Artwork%29.png/revision/latest?cb=20150919133639",
+    img: "./images/Kristie.png",
     moldura: "card.png"
   },
   {
     Nome: "Kathinja",
     Atributos: { HP: 30, Força: 12, Defesa: 60, Elemento: 90 },
-    img: "https://static.wikia.nocookie.net/mana/images/d/d5/Kathinja_%28LoM_Artwork%29.png/revision/latest?cb=20150919133630",
+    img: "./images/Kathinja.png",
     moldura: "card.png"
   },
   {
-    Nome: "Faeries",
+    Nome: "Faerie",
     Atributos: { HP: 40, Força: 30, Defesa: 12, Elemento: 60 },
-    img: "https://static.wikia.nocookie.net/mana/images/a/a8/Faerie_%28LoM_Artwork%29.png/revision/latest?cb=20150919133259",
+    img: "./images/Faerie.png",
     moldura: "card.png"
   },  
   {
-    Nome: "Sproutlings",
+    Nome: "Sproutling",
     Atributos: { HP: 30, Força: 40, Defesa: 30, Elemento: 40 },
-    img: "https://static.wikia.nocookie.net/mana/images/f/f3/Sproutling_%28LoM_Artwork%29.png/revision/latest?cb=20150919134402",
+    img: "./images/Sproutling.png",
     moldura: "card.png"
   },
   {
     Nome: "Irwin",
     Atributos: { HP: 65, Força: 70, Defesa: 60, Elemento: 60 },
-    img: "https://superromobrothers.files.wordpress.com/2017/11/irwin.png?w=311&h=504",
+    img: "./images/Irwin.png",
     moldura: "card.png"
   },
   {
     Nome: "Matilda",
     Atributos: { HP: 65, Força: 40, Defesa: 40, Elemento: 40 },
-    img: "https://superromobrothers.files.wordpress.com/2017/11/matilda_young.png?w=306&h=504",
+    img: "./images/Matilda_young.png",
     moldura: "card.png"
   },
   {
     Nome: "Belle",
     Atributos: { HP: 30, Força: 30, Defesa: 30, Elemento: 75 },
-    img: "https://static.wikia.nocookie.net/mana/images/6/67/Belle_%28LoM_Artwork%29.png/revision/latest?cb=20150919132657",
+    img: "./images/Belle.png",
     moldura: "card.png"
   },
   {
     Nome: "Pokiehl",
     Atributos: { HP: 70, Força: 70, Defesa: 70, Elemento: 70 },
-    img: "https://static.wikia.nocookie.net/mana/images/e/eb/Pokiehl_%28LoM_Artwork%29.png/revision/latest?cb=20150919134129",
+    img: "./images/Pokiehl.png",
     moldura: "card.png"
   },
   {
     Nome: "Mephianse",
     Atributos: { HP: 45, Força: 45, Defesa: 30, Elemento: 75 },
-    img: "https://static.wikia.nocookie.net/mana/images/0/0a/Mephianse_%28LoM_Artwork%29.png/revision/latest?cb=20150919133841",
+    img: "./images/Mephianse.png",
     moldura: "card.png"
   },
   {
     Nome: "Flameshe",
     Atributos: { HP: 65, Força: 35, Defesa: 35, Elemento: 65 },
-    img: "https://static.wikia.nocookie.net/mana/images/1/18/Flameshe_%28LoM_Artwork%29.png/revision/latest?cb=20150919133313",
+    img: "./images/Flameshe.png",
     moldura: "card.png"
   },
   {
     Nome: "Monique",
     Atributos: { HP: 60, Força: 35, Defesa: 35, Elemento: 55 },
-    img: "https://static.wikia.nocookie.net/mana/images/8/81/Monique_%28LoM_Artwork%29.png/revision/latest?cb=20150911134355",
+    img: "./images/Monique.png",
     moldura: "card.png"
   }
 ];
